46a5d52ce924107f2c47ad88db41c633e105527e
Tobias Ricken de Medeiros <tobiasrdm@hotmail.com>
2024-04-08 10:17:37 -0300

    Lendo o novo formato HTML do NuBank

--- src/main/java/br/com/trm/categorizadorautomaticodedespesas/LeitorDeDespesasAPatirDeLinhas.java
+++ src/main/java/br/com/trm/categorizadorautomaticodedespesas/LeitorDeDespesasAPatirDeLinhas.java
@@ -7,37 +7,34 @@ import java.util.List;
 
 public class LeitorDeDespesasAPatirDeLinhas {
 	public List<Despesa> lerDespesas(String stringDespesas) {
-		//Substitui todos os Enter por Tab, colocando todos os registros em uma única linha
-		stringDespesas = stringDespesas.replace("\n", "\t");
+		// Substitui todos os Enters por Tab, colocando todos os registros em uma única linha
+		stringDespesas = stringDespesas.replace("\n\n", "\t");
 		List<String> campos = Arrays.asList(stringDespesas.split("\t"));
-		int umNovoRegistroACadaQuantosCampos = 3;
 		List<String> valoresDosCampos = new ArrayList<>();
 		List<List<String>> valoresDosRegistros = new ArrayList<>();
 		for (int campo = 0; campo < campos.size(); campo++) {
-			int registroDoCampoAnterior = ((campo - 1) / umNovoRegistroACadaQuantosCampos);
-			int registroAtual = (campo / umNovoRegistroACadaQuantosCampos);
-			int registroDoProximoCampo = ((campo + 1) / umNovoRegistroACadaQuantosCampos);
-			if (registroAtual != registroDoCampoAnterior) {
+			String valorDoCampoAtual = campos.get(campo); 
+			valoresDosCampos.add(valorDoCampoAtual);
+			// Verificando se é o último campo do registro
+			if (valorDoCampoAtual != null && valorDoCampoAtual.startsWith("R$")) {
+				valoresDosRegistros.add(valoresDosCampos);
 				// Inicia novo registro
 				valoresDosCampos = new ArrayList<>();
-			}
-			valoresDosCampos.add(campos.get(campo));
-			// Verificando se é o última campo do registro
-			if (registroDoProximoCampo != registroAtual) {
-				valoresDosRegistros.add(valoresDosCampos);
 			}
 		}
 		List<Despesa> despesas = new ArrayList<>();
 		for (List<String> valoresDasColunas : valoresDosRegistros) {
+			int indexColunaValor = valoresDasColunas.size() -1;//Última coluna
+			int indexColunaDescricao = indexColunaValor -1;//Penúltima coluna
 			Despesa despesa = new Despesa();
-			despesa.setDescricao(valoresDasColunas.get(1).trim());
+			despesa.setDescricao(valoresDasColunas.get(indexColunaDescricao).trim());
 			if (despesa.getDescricao().endsWith(" -")) {
 				// Removendo o sufixo " -" da descrição
 				despesa.setDescricao(despesa.getDescricao().substring(0, despesa.getDescricao().lastIndexOf(" -")));
 			}
 			// Remove os pontos agrupadores de milhar e depois substitui vírgula
 			// decimal por ponto decimal
-			despesa.setValor(new BigDecimal(valoresDasColunas.get(2).replace(".", "").replace(",", ".")));
+			despesa.setValor(new BigDecimal(valoresDasColunas.get(indexColunaValor).replace("R$ ", "").replace(".", "").replace(",", ".")));
 			despesa.setRealizado("NÃO");
 			if (despesa.getValor().compareTo(BigDecimal.ZERO) >= 0) {
 				// Só coloca na lista se o valor da despesa for maior ou igual a
