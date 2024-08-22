95ec937ffb8886aeb0f528bf2ae6495ef4393b81
Tobias Ricken de Medeiros <tobiasrdm@hotmail.com>
2024-08-22 14:04:50 -0300

    Suporte para despesas copiadas do PDF da Fatura NuBank

--- src/main/java/br/com/trm/categorizadorautomaticodedespesas/LeitorDeDespesasAPatirDeLinhas.java
+++ src/main/java/br/com/trm/categorizadorautomaticodedespesas/LeitorDeDespesasAPatirDeLinhas.java
@@ -6,7 +6,16 @@ import java.util.Arrays;
 import java.util.List;
 
 public class LeitorDeDespesasAPatirDeLinhas {
+	
 	public ReceitasEDespesas lerDespesas(String stringDespesas) {
+		if (stringDespesas.indexOf("\t") == -1) {
+			return lerDespesasFromFaturaPdfNuBank(stringDespesas);
+		} else {
+			return lerDespesasDoHtmlAcessoPeloSiteNuBank(stringDespesas);
+		}
+	}
+	
+	private ReceitasEDespesas lerDespesasDoHtmlAcessoPeloSiteNuBank(String stringDespesas) {
 		// Substitui todos os Enters por Tab, colocando todos os registros em uma única linha
 		stringDespesas = stringDespesas.replace("\n\n", "\t");
 		List<String> campos = Arrays.asList(stringDespesas.split("\t"));
@@ -40,6 +49,32 @@ public class LeitorDeDespesasAPatirDeLinhas {
 				receitasEDespesas.getDespesas().add(despesa);
 			} else {
 				receitasEDespesas.getReceitas().add(despesa);
+			}
+		}
+		return receitasEDespesas;
+	}
+	
+	private ReceitasEDespesas lerDespesasFromFaturaPdfNuBank(String stringDespesas) {
+		ReceitasEDespesas receitasEDespesas = new ReceitasEDespesas();
+		List<String> registros = Arrays.asList(stringDespesas.split("\n"));
+		for (String registro: registros) {
+			Despesa despesa = new Despesa();
+			int inicioDoCampoValor = registro.indexOf("-R$ ");
+			if (inicioDoCampoValor == -1) {
+				inicioDoCampoValor = registro.indexOf("R$ ");
+			}
+			int inicioDoCampoDescricao = "01 AGO".length();
+			despesa.setDescricao(registro.substring(inicioDoCampoDescricao, inicioDoCampoValor -1).trim());
+			// Remove os pontos agrupadores de milhar e depois substitui vírgula
+			// decimal por ponto decimal
+			despesa.setValor(new BigDecimal(registro.substring(inicioDoCampoValor).replace("R$ ", "").replace(".", "").replace(",", ".")));
+			despesa.setRealizado("NÃO");
+			if (despesa.getValor().compareTo(BigDecimal.ZERO) != 0) {
+				if (isDespesa(despesa)) {
+					receitasEDespesas.getDespesas().add(despesa);
+				} else {
+					receitasEDespesas.getReceitas().add(despesa);
+				}
 			}
 		}
 		return receitasEDespesas;
